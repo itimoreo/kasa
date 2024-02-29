eba10745891f35a4c2cc1b80a5d7dcc0c4eb97cf
itimoreo <pol.lopezpro@gmail.com>
2024-02-29 15:02:24 +0100

    some changes

--- src/components/Error404.js
+++ src/components/Error404.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import '../css/Error404.css';
 
 const Error404 = () => {
     return (
         <div>
-            <h1>404 Error</h1>
-            <p>Page not found</p>
+            <h1 className='error-Title'>404</h1>
+            <p className='error-Txt'>Oups! La page que vous demandez n'existe pas.</p>
         </div>
     );
 };
