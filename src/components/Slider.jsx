d2f44dc0b8032c722a2687201512e020f6d63da8
itimoreo <pol.lopezpro@gmail.com>
2024-03-16 13:58:28 +0100

    ajout des flèches pour slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,57 +1,72 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect, useRef } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import Slider from "react-slick";
-import { useParams, useNavigate } from 'react-router-dom';
-import data from '../json/appart.json'; // Importez vos données JSON
-
-// Importez les fichiers CSS de slick-carousel
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import data from "../json/appart.json";
+import "../css/Slider.css";
 
-// Créez des composants pour vos flèches
 function NextArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-      <div
-        className={className}
-        style={{ ...style, display: "block", position: "absolute", top: "50%", right: "10px", transform: "translateY(-50%)", zIndex: 2, background: "none" }}
-        onClick={onClick}
+  const { className, style, onClick } = props;
+  return (
+    <div
+    className={`${className} custom-next-arrow`}
+    style={{ ...style, display: 'block', right: '25px', top: '50%',}}
+    onClick={onClick}
+  >
+      <svg
+        width="48"
+        height="80"
+        viewBox="0 0 48 80"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
       >
-        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" viewBox="0 0 16 16">
-          <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-5.147-5.146a.5.5 0 0 1 .708-.708l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
-        </svg>
-      </div>
-    );
-  }
-  
-  function PrevArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-      <div
-        className={className}
-        style={{ ...style, display: "block", position: "absolute", top: "50%", left: "10px", transform: "translateY(-50%)", zIndex: 2, background: "none" }}
-        onClick={onClick}
+        <path
+          d="M0.960022 72.3458L8.04002 79.4258L47.64 39.8258L8.04002 0.22583L0.960022 7.30583L33.48 39.8258L0.960022 72.3458Z"
+          fill="white"
+        />
+      </svg>
+    </div>
+  );
+}
+
+function PrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={`${className} custom-prev-arrow`}
+      style={{ ...style, display: 'block', left: '25px', top: '50%', zIndex: '1'}}
+      onClick={onClick}
+    >
+      <svg
+        width="48"
+        height="80"
+        viewBox="0 0 48 80"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
       >
-        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" viewBox="0 0 16 16">
-          <path fillRule="evenodd" d="M15 8a.5.5 0 0 1-.5.5H2.707l5.147 5.146a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708.708L2.707 7.5H14.5A.5.5 0 0 1 15 8z"/>
-        </svg>
-      </div>
-    );
-  }
+        <path
+          d="M47.04 7.78312L39.92 0.703125L0.359985 40.3031L39.96 79.9031L47.04 72.8231L14.52 40.3031L47.04 7.78312Z"
+          fill="white"
+        />
+      </svg>
+    </div>
+  );
+}
 
 const CardPageSlider = () => {
   const { id } = useParams();
   const navigate = useNavigate();
   const [appartment, setAppartment] = useState(null);
-  const [currentSlide, setCurrentSlide] = useState(0);
+  const sliderRef = useRef(null);
 
   useEffect(() => {
-    // Trouvez l'appartement correspondant à l'ID
-    const foundAppartment = data.find(appartment => appartment.id === id);
+    const foundAppartment = data.find((appartment) => appartment.id === id);
 
     if (foundAppartment) {
       setAppartment(foundAppartment);
     } else {
-      navigate('/error'); // Redirigez vers une page d'erreur si l'ID n'est pas valide
+      navigate("/error");
     }
   }, [id, navigate]);
 
@@ -63,41 +78,30 @@ const CardPageSlider = () => {
     slidesToScroll: 1,
     nextArrow: <NextArrow />,
     prevArrow: <PrevArrow />,
-    beforeChange: (oldIndex, newIndex) => setCurrentSlide(newIndex),
     adaptiveHeight: true,
-    // centerMode: true, // Commentez ou supprimez cette ligne
+    arrows: true,
   };
 
-  // Ajoutez un style pour les images
-  const imageStyle = {
-    width: '100%', // Assurez-vous que l'image prend toute la largeur disponible
-    maxHeight: '500px', // Définissez une hauteur maximale pour les images
-    objectFit: 'cover' // Assurez-vous que l'image couvre toute la surface disponible
-  };
-
-  // Style pour le texte
-  const textStyle = {
-    position: 'absolute',
-    bottom: '10px',
-    left: '50%',
-    transform: 'translateX(-50%)',
-    color: '#fff',
-    fontSize: '20px',
-    textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' // Pour rendre le texte plus lisible sur les images
-  };
-
-  return appartment ? (
-    <div style={{ position: 'relative' }}>
-      <Slider {...settings}>
-        {appartment.pictures.map((picture, index) => (
-          <div key={index}>
-            <img src={picture} alt="" style={imageStyle} />
-          </div>
-        ))}
+  return (
+    <div style={{ position: "relative" }}>
+      <Slider ref={sliderRef} {...settings}>
+        {appartment &&
+          appartment.pictures.map((picture, index) => (
+            <div key={index}>
+              <img
+                src={picture}
+                alt={`Slide ${index}`}
+                style={{
+                  width: "100%",
+                  maxHeight: "500px",
+                  objectFit: "cover",
+                }}
+              />
+            </div>
+          ))}
       </Slider>
-      <p style={textStyle}>{currentSlide + 1} / {appartment.pictures.length}</p>
     </div>
-  ) : null;
+  );
 };
 
 export default CardPageSlider;
